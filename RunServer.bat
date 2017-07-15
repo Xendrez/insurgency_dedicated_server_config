24323ba1791e6191fc897c4c2ee4ccbf6d58f6ca
Xendrez <stephanswrt@gmail.com>
2017-07-15 15:35:11 +0200

    Setup like: https://steamcommunity.com/app/222880/discussions/3/620695877135274234/

--- RunServer.bat
+++ RunServer.bat
@@ -1,7 +1,7 @@
-srcds.exe -game insurgency -console +map "khanashin_b8 hunt" +sv_lan 0
-
 @echo off
 title Insurgency Server
 :start
-start /wait /abovenormal srcds.exe -game insurgency -console +map district_coop +maxplayers 32 +sv_lan 0 -port 27020
+start /wait /abovenormal srcds.exe -game insurgency -console +map district_coop +maxplayers 32 +sv_lan 1 -port 27020
 goto start
+
+REM see https://steamcommunity.com/app/222880/discussions/3/620695877135274234/
